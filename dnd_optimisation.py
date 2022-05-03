1ffd700a236bc116e432e13af8fed496c2801c64
Eleni Vasilaki <e.vasilaki@gmail.com>
2022-05-03 17:36:19 +0300

    converted to OOP

--- dnd_optimisation.py
+++ dnd_optimisation.py
@@ -1,6 +1,82 @@
 from pickle import TRUE
 import scipy
 
+class Character:
+    def __init__(self,str,dex,con,int,wis,cha):
+        import numpy as np
+        self.ability_score= np.array([str,dex,con,int,wis,cha])
+        self.modifier = np.floor((self.ability_score - 10)/2)
+    
+    def set_level(self,char_level=1):
+        import numpy as np
+        self.level=char_level
+        self.proficiency_bonus=1+np.ceil(char_level/4)
+
+    def return_ability_score(self,ability_name):
+        self.ability=['STR', 'DEX', 'CON','INT','WIS', 'CHA']
+        return(self.ability_score[self.ability.index(ability_name)])
+
+    def set_other_bonus(self,ob=0):
+        self.other_bonus=ob
+
+    def set_advantages(self,advantage=0,luck_point=0,elven_accuracy=0,hexblade_curse=0):
+        #checks
+        advantage=(advantage==1)
+        luck_point=(luck_point==1)
+        elven_accuracy=(elven_accuracy==1)
+        hexblade_curse=(hexblade_curse==1)
+        #assignments
+        self.advantage=advantage
+        self.luck_point=luck_point
+        self.elven_accuracy=elven_accuracy
+        self.hexblade_curse=hexblade_curse
+        
+        #calcuate probability of critical hit
+        __critit=1/20*(1-hexblade_curse)+2/20*hexblade_curse  
+        p=advantage+luck_point+advantage*elven_accuracy+1
+        self.critit=round(1-(1-__critit)**p,5)
+
+
+    def set_hex(self,hex=0):
+        hex=(hex==1)
+        self.hex=3.5*hex
+
+        
+
+    def set_eldrich_blast(self,eb=0,max_eb_dice=0,agonising_eb=0,eb_bonus=0):
+        import math
+        from statistics import mean
+        eb=(eb==1)
+        #if eb=0 everything else should be 0
+        self.eldrich_blast=eb
+        self.eldrich_blast_beams=eb*min(math.floor(self.level/5)+1,4)
+        self.eldrich_blast_hit_bonus=eb*eb_bonus #example rod of pact keeper
+        self.eldrich_blast_mean_damage=eb*(mean(range(1,max_eb_dice+1,1))+self.modifier[self.ability.index("CHA")]*agonising_eb)
+        self.eldirch_blast_hit=eb*(self.modifier[self.ability.index("CHA")]+self.proficiency_bonus+self.eldrich_blast_hit_bonus)
+    
+
+    def hit_opponent_probability(self,opponent_AC=1,modifiers=0):
+        p_miss= min(max((opponent_AC-modifiers-1),1),19)/20
+        p=self.advantage+self.luck_point+self.advantage*self.elven_accuracy+1
+        p_hit=round(1-(p_miss)**p,5)
+        p_hit=round(p_hit,5)
+        return p_hit
+
+    def total_average_damage(self,opponent_AC=1,modifiers=0,repetitions=1,average_damage_per_hit=0):
+        import scipy.stats
+        total_damage=0
+        index_repetitions = 1
+        p_hit=self.hit_opponent_probability(opponent_AC,modifiers)
+        p_crit_hit=self.critit 
+        #The logic of the loop below is the following. I am calculating the probability of having 1 repetition of the weapon or spell succesful (say 1 eldirch blast ray) times the damage that the weapon will do plus the probability of having two repetitions succesfull times twice the damage etc.
+        while index_repetitions < (repetitions+1):
+              total_damage+=scipy.stats.binom.pmf(index_repetitions,repetitions,p_hit)*index_repetitions*average_damage_per_hit+scipy.stats.binom.pmf(index_repetitions,repetitions,p_crit_hit)*index_repetitions*average_damage_per_hit
+              index_repetitions += 1
+        total_damage=round(total_damage,5)
+        return total_damage
+
+
+
 def probability_of_crtical_hit(advantage,luck_point,elven_accuracy,hexblade_curse):
     #advantage,luck_point,elven_accuracy,hexblade_curse take values 1 or 0.
     #if they are given a value different to 1, this is converted to 0 in the checks.
